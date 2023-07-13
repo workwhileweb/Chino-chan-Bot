3379d36002507c09584ee8cad2254c0c93ecc821
workwhileweb <workwhileweb@gmail.com>
2023-07-13 10:19:21 +0700

    xxx

--- config/config.js
+++ config/config.js
@@ -1,15 +1,16 @@
 const NodeCache = require('node-cache')
 
 const config = {
-    prefix: '/',
-    openaikey: '',
-    cache: new NodeCache({
-        checkperiod: 10000,
-        deleteOnExpire: true
-    }),
-    ratelimit: new Map(),
-    commands: new Map(),
-    aliases: new Map()
+  prefix: '/',
+  openaikey: require('C:\\Users\\workw\\Downloads\\txt\\12072023\\config.json')
+    .openai.token,
+  cache: new NodeCache({
+    checkperiod: 10000,
+    deleteOnExpire: true
+  }),
+  ratelimit: new Map(),
+  commands: new Map(),
+  aliases: new Map()
 }
 
 module.exports = config
--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const client = new Client({ online: true })
 const fs = require('fs')
 
 client.on('ready', bot => {
-    for (const file of fs.readdirSync('./handlers')) {
-        if (!file.endsWith('.js')) continue
-        require(`./handlers/${file}`)(bot)
-    }
+  for (const file of fs.readdirSync('./handlers')) {
+    if (!file.endsWith('.js')) continue
+    require(`./handlers/${file}`)(bot)
+  }
 
-    console.log(`${bot.user.username} đã online.`)
+  console.log(`${bot.user.username} đã online.`)
 })
 
-client.login(require('./config/fbstate.json'), true)
+const fb = require('C:/Users/workw/Downloads/txt/12072023/fbstate.json')
+client.login(fb, true)
